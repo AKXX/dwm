f937ba178f364a914895f8c73957c94953d04ba5
AK <ak@redchan.it>
2018-12-02 19:09:43 -0500

    added font awesome for some icons to appear in status

--- config.h
+++ config.h
@@ -5,7 +5,9 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Misc Tamsyn:size=13" };
+static const char *fonts[]          = { "Misc Tamsyn:size=13", 
+                                         "FontAwesome:size=11" 
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
